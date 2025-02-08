ff1e99f4961a4ad766b8dca05b1d575861b534c9
CamileBarros <camile.barros@avenue.us>
2025-02-08 12:01:50 -0300

    Add .gitignore and enhance main.go for dynamic port and authorization handling

--- main.go
+++ main.go
@@ -4,19 +4,38 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", HomeHandler).Methods("GET")
-	http.Handle("/", r)
-	fmt.Println("Servidor rodando na porta 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // Porta padrão se não especificada
+	}
 
+	r := mux.NewRouter()
+	r.HandleFunc("/my-first-go-project", HomeHandler).Methods("GET")
+	http.Handle("/", r)
+	fmt.Printf("Servidor rodando na porta %s\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello world!"))
+	authHeader := r.Header.Get("Authorization")
+	if authHeader == "" {
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte("Autorização necessária"))
+		return
+	}
+
+	if len(authHeader) > 7 && authHeader[:7] == "Bearer " {
+		w.Write([]byte("Tipo de autorização: Bearer Token"))
+	} else if len(authHeader) > 5 && authHeader[:5] == "Basic" {
+		w.Write([]byte("Tipo de autorização: Basic Auth"))
+	} else {
+		w.Write([]byte("Tipo de autorização desconhecido"))
+	}
 }
